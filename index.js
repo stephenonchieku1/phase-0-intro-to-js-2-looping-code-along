8dd536208b9d12edb648a62c9c1f8e5326dc7487
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-12 23:08:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+    console.log(positiveNumber);
+    function writeCards (names, event) {
+      const messages = [];
+      for (let i = 0; i < names.length; i++){
+          messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      }
+      return messages;
+  }
+  function countDown(positiveNumber){
+      while (positiveNumber >= 0){
+          console.log (positiveNumber);
+      positiveNumber--;
+      }
+  }
