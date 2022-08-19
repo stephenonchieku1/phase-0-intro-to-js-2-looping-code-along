7693750bc066353199afb96f861cfcd34c655b61
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-19 17:42:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
 
-    function writeCards (names, event) {
-      const messages = [];
-      for (let i = 0; i < names.length; i++){
-          messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-      }
-      return messages;
-  }
-  function countDown(positiveNumber){
-      while (positiveNumber >= 0){
-          console.log (positiveNumber);
-      positiveNumber--;
-      }
-  }
+ function writeCards (names=["Guadalupe", "Ollie", "Aki"], events="surprise") {
+let messages = [];
+for (let i = 0; i < names.length; i++){
+      messages[1]=(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+}
+return messages;
+}
+console.log(writeCards());
+function countDown(positiveNumber){
+  while (positiveNumber >= 0){
+   console.log (positiveNumber);
+positiveNumber--;
+}
+}
+countDown(10)
